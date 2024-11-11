28c3eb714895134b521eebae893d3ab0b8641646
Santos Vilanculos <95357132+SantosVilanculos@users.noreply.github.com>
2024-11-11 04:09:06 +0200

    chore: build other pages too

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,16 @@ import glsl from 'vite-plugin-glsl';
 
 export default defineConfig({
   base: './',
-  plugins: [glsl()]
+  plugins: [glsl()],
+  build: {
+    rollupOptions: {
+      input: {
+        index: './index.html',
+        example1: './example1.html',
+        example2: './example2.html',
+        example3: './example3.html',
+        example4: './example4.html'
+      }
+    }
+  }
 });
